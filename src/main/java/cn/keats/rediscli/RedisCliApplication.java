ed40984b9c853286f3118bf64702d74794e2a191
keats_coder <2455629678@qq.com>
2020-03-31 19:34:42 +0800

    Initial commit

---
+++ src/main/java/cn/keats/rediscli/RedisCliApplication.java
@@ -0,0 +1,13 @@
+package cn.keats.rediscli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCliApplication.class, args);
+    }
+
+}
